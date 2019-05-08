e3c33f05790918feea9b8854064db1d7b5a3cf43
juho <wngh1187@naver.com>
2019-05-08 17:36:08 +0900

    여러 주가들을 사용하여 lable 생성

--- Technical_Indicator_stock_predict/02_make_label.py
+++ Technical_Indicator_stock_predict/02_make_label.py
@@ -1,39 +1,45 @@
-#!/usr/bin/python
 #-*- coding: cp949 -*-
 #-*- coding: utf-8 -*- 
 
-#	저장된 엑셀을 불러온다.
-#	일자, 시가, 고가, 저가, 종가, 거래량은 data로 저장
-#	MACD, EMA, Momentum, SlowK, SlowD, ROC, William_R, A/D, RSI, OBV, upperband, lowerband는 기술적 지표로서 학습에 사용될 X_label로 저장
-#	전날대비 증가량은 y_label로 저장
+#	data 폴더에 저장된 엑셀들을 불러온다.
+#	일자, 시가, 고가, 저가, 종가, 거래량은 주가이름_data로 저장
+#	MACD, EMA, Momentum, SlowK, SlowD, ROC, William_R, A/D, RSI, OBV, upperband, lowerband는 기술적 지표로서 학습에 사용될 주가이름_X_label로 저장
+#	전날대비 증가량은 주가이름_y_label로 저장
 
 from openpyxl import load_workbook
 import numpy as np
+import os
 
-if __name__ == '__main__':
-	sk_data = load_workbook(filename='chart.xlsx')
+if __name__ == '__main__': 
+	
+	for root, dirs, files in os.walk('./data'):
+		for file in files:
+			filename = file[:-5]
+			
+			filedata = load_workbook('./data/'+file)
 
-	sheet1 = sk_data['Sheet1']
+			sheet1 = filedata['Sheet1']
 
-	sheet2 = sk_data.active
+			sheet2 = filedata.active
 
-	print(sheet1['D4'].value)
-	print(sheet2['D4'].value)
-	datas = []
-	x_labels = []
-	y_labels = []
+			print(sheet1['D4'].value)
+			print(sheet2['D4'].value)
+			datas = []
+			x_labels = []
+			y_labels = []
 
-	for i in sheet1.rows:
-		data=[]
+			for i in sheet1.rows:
+				data=[]
 
-		for d in i:
-			data.append(d.value)
+				for d in i:
+					data.append(d.value)
 
 		
-		datas.append(data[:6])
-		x_labels.append(data[6:-1])
-		y_labels.append(data[-1])
+				datas.append(data[:6])
+				x_labels.append(data[6:-1])
+				y_labels.append(data[-1])
 
-	np.save('sk_data.npy',datas)
-	np.save('sk_x_label.npy',x_labels)
-	np.save('sk_y_label.npy',y_labels)
+			np.save('./data_label/'+filename+'_data.npy',datas)
+			np.save('./data_label/'+filename+'_x_label.npy',x_labels)
+			np.save('./data_label/'+filename+'_y_label.npy',y_labels)
+			
